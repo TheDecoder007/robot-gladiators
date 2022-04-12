7f3f98287097efac7f7b5287156663af96e3e29c
Ryan Murphy <TheDecoder007@gmail.com>
2022-04-12 15:22:51 -0400

    code for health updates

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,24 +12,40 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
-    // alert players they are starting the round
+// alert players they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
-    //subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable
+//subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable
     enemyHealth = enemyHealth - playerAttack;
 
-    //log a resulting message to the console so we know it worked
+//log a resulting message to the console so we know it worked
 console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining "
 );
 
-    //subtract the value of enemyAttack from the value of playerHealth and use that result to update the value in the playerHealth variable
+//check enemies health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+//subtract the value of enemyAttack from the value of playerHealth and use that result to update the value in the playerHealth variable
     playerHealth = playerHealth - enemyAttack;
 
-    //log a resulting message to the console so we know it worked
+//log a resulting message to the console so we know it worked
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " how has " + playerHealth + " health remaining "
     );
+
+//check players health
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+        }
+        else {
+            window.alert(playerName + " still has " + playerHealth + "health left.");
+        }
 };
 
 fight()
