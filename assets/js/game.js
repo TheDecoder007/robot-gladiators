e1f03fc571f38ab1adf976319eb520d2bd530197
Ryan Murphy <TheDecoder007@gmail.com>
2022-04-13 17:20:23 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,12 @@ var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+}
+
 console.log(enemyNames);
 console.log(enemyNames.length);
 console.log(enemyNames[0]);
@@ -23,14 +29,17 @@ var fight = function(enemyName) {
          if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Sucka!');
     
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+ // remove enemy's health by subtracting the amount set in the playerAttack variable
+
+   var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -49,7 +58,10 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -82,7 +94,7 @@ for (var i = 0; i < enemyNames.length; i++) {
       var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
 
     fight(pickedEnemyName);
 
@@ -99,7 +111,7 @@ for (var i = 0; i < enemyNames.length; i++) {
     }
   }
   else {
-    window.alert('You have lost your robot in battle! Game Over!');
+    window.alert("You went out like a bitch and lost your robot");
     break;
   }
 }
